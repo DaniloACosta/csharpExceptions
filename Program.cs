193366572d83820de1437baa2c6a18d007857edc
Danilo C <danilo.c@outlook.com.br>
2019-01-27 10:29:24 -0200

    Exception generica e throw.

--- Program.cs
+++ Program.cs
@@ -1,37 +1,51 @@
 ﻿using System;
 
-namespace csharpExceptions {
-    class Program {
-        static void Main (string[] args) {
-            try{
-                Metodo ();
-            }catch(DivideByZeroException e){
+namespace csharpExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Não é possivél dividir por 0");
+            }
+            catch (Exception e)
+            {
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
             }
-
-            Console.ReadLine ();
-
+            Console.ReadLine();
         }
 
-        public static int Dividir (int numero, int divisor) {
-            ContaCorrente c = null;
-            Console.WriteLine(c.Saldo);
-            
-            return numero / divisor;
-        }
+        public static int Dividir(int numero, int divisor)
+        {
+            //ContaCorrente c = null;
+            //Console.WriteLine(c.Saldo);
 
-        static void Metodo () {
-            try{
-                TestaDivisao (0);
-            }catch(NullReferenceException e){
-                Console.WriteLine(e.Message);
-                Console.WriteLine(e.StackTrace);
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine(String.Format("Exceção ao tentar dividir {0} por {1}", numero, divisor));
+                throw;
             }
         }
 
-        static void TestaDivisao (int divisor) {
-            Dividir (10, divisor);
+        static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
         }
     }
 }
